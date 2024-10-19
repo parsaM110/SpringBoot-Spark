a0c5ab5f6ee72ac7ebb903785ad00c9afc166a80
parsaM110 <pmomeni80@gmail.com>
2024-10-19 20:38:02 +0330

    initial commit

---
+++ src/main/java/com/example/springbootspark/SpringBootSparkApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootspark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSparkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSparkApplication.class, args);
+    }
+
+}
